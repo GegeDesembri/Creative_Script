6712834574992f8a7aa220e520e02d4544b5b6dd
Gege Desembri <GegeDesembri.643.UM@gmail.com>
2021-07-02 09:01:09 +0700

    variable-js

---
+++ web/variable-js/javascript/variable.js
@@ -0,0 +1,11 @@
+let firstName = "Gege"
+let lastName = "Desembri"
+
+// Syntax per Line
+document.writeln(firstName)
+document.writeln(lastName)
+document.writeln("</br>")
+
+// Syntax One Line
+document.writeln(firstName + ' ' + lastName)
+document.writeln("</br>")
