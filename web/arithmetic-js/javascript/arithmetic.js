363db6cb00ff69639b7b164c52381b3a500a2883
Gege Desembri <GegeDesembri.643.UM@gmail.com>
2021-07-02 09:32:17 +0700

    arithmetic-js

---
+++ web/arithmetic-js/javascript/arithmetic.js
@@ -0,0 +1,5 @@
+let num1 = 6
+let num2 = 3
+let result = num1 + num2
+
+document.writeln("<p>" + num1 + " + " + num2 + " = " + result + "</p>")
--- web/variable-js/javascript/variable.js
+++ web/variable-js/javascript/variable.js
@@ -1,11 +1,20 @@
 let firstName = "Gege"
 let lastName = "Desembri"
+let fullName = firstName + ' ' + lastName
 
 // Syntax per Line
+document.writeln("Syntax/Line")
+document.writeln("</br>")
 document.writeln(firstName)
 document.writeln(lastName)
 document.writeln("</br>")
+document.writeln(fullName)
+document.writeln("</br>")
 
 // Syntax One Line
+document.writeln("\nOneSyntax")
+document.writeln("</br>")
 document.writeln(firstName + ' ' + lastName)
+document.writeln("</br>")
+document.writeln(fullName)
 document.writeln("</br>")
