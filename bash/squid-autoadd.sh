826cffb410162d9df9977409186e02b650663980
Gege Desembri <62755680+GegeDesembri@users.noreply.github.com>
2022-11-19 11:03:01 +0700

    Add files via upload

--- bash/squid-autoadd.sh
+++ bash/squid-autoadd.sh
@@ -26,12 +26,33 @@ function loop_range(){
 	user_prefix="$1"
 	range_min="$2"
 	range_max="$3"
+	port_use="$4"
+	
+	if [[ "${port_use}" == '' ]]; then
+		spawn_bin="/usr/local/bin/squid-add-user"
+	else
+		if [[ $(cat /etc/squid/squid.conf | grep -wce "^include ")  == 0 ]]; then
+			echo -e 'include /etc/squid/conf.d/*.conf' >> /etc/squid/squid.conf
+		fi
+		default_port="$(cat /etc/squid/squid.conf | grep "http_port" | cut -d ' ' -f 2)"
+		conf_perport="/etc/squid/conf.d/${port_use}.conf"
+		passwd_perport="/etc/squid/conf.d/${port_use}"
+		spawn_bin="/usr/local/bin/squid-add-user-${port_use}"
+		cp /etc/squid/squid.conf ${conf_perport}
+		touch ${passwd_perport}
+		cp /usr/local/bin/squid-add-user ${spawn_bin}
+		chmod +x ${spawn_bin}
+		sed -i "s|${default_port}|${port_use}|g" ${conf_perport}
+		sed -i "s|/etc/squid/passwd|${passwd_perport}|g" ${conf_perport}
+		sed -i "/^include /d" ${conf_perport}
+		sed -i "s|/etc/squid/passwd|${passwd_perport}|g" ${spawn_bin}
+	fi
 	
 	for USERPASS in $(eval echo "{$range_min..$range_max}"); do
-		autocreate "${user_prefix}${USERPASS}"
+		autocreate "${spawn_bin}" "${user_prefix}${USERPASS}"
 		echo "${user_prefix}${USERPASS}" | tee -a /root/squid_generated.txt
 		sleep 0.5
 	done
 }
 
-loop_range $1 $2 $3
+loop_range $1 $2 $3 $4
