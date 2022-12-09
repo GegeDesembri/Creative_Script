cdae4fa948747242554bdaf51d151be51e63857d
Gege Desembri <62755680+GegeDesembri@users.noreply.github.com>
2022-12-09 20:58:35 +0700

    Update dante-autoadd.sh

--- bash/other/dante-autoadd.sh
+++ bash/other/dante-autoadd.sh
@@ -5,7 +5,7 @@ user_prefix="$1"
 range_min="$2"
 range_max="$3"
 for USERPASS in $(eval echo "{$range_min..$range_max}"); do
-	dante-add "${user_prefix}${USERPASS}"
+	dante-add "${user_prefix}${USERPASS}" "${user_prefix}${USERPASS}"
 	echo "${user_prefix}${USERPASS}" | tee -a /root/dante_generated.txt
 	sleep 0.5
 done
